1af61e63f525ac526e46e0f703ed4c9c5056ff7d
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-11 12:35:49 +0100

    added imports

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import cors from "cors";
 import express from "express";
+import dotenv from 'dotenv'
+import log from "./src/logger"
+dotenv.config({ path: './config/.env' });
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT
+const base = process.env.BASE
 
 app.use(cors()); 
 app.use(express.json());
 
 app.listen(port, () => {
-  console.info(`[INFO] Server Started on PORT: ${port}`);
+ log.info(`[INFO] Server Started on PORT: ${port}`);
+  log.info(`Server listening at ${base}`)
 })
