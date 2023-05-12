514d6a6dafc0745ae94597ddf77971e613118b32
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-12 22:36:48 +0100

    added routes

--- index.ts
+++ index.ts
@@ -2,7 +2,10 @@ import cors from "cors";
 import express from "express";
 import dotenv from 'dotenv'
 import log from "./src/logger"
+import connect from "./config/connect";
+import routes from "./src/routes"
 dotenv.config({ path: './config/.env' });
+
 
 const app = express();
 const port = process.env.PORT
@@ -14,4 +17,7 @@ app.use(express.json());
 app.listen(port, () => {
  log.info(`[INFO] Server Started on PORT: ${port}`);
   log.info(`Server listening at ${base}`)
+
+  connect()
+  routes(app)
 })
