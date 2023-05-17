8c56cfa0c21a01383774feff7594f06c863bee58
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-17 22:01:14 +0100

    fix model

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -4,13 +4,13 @@ import dotenv from 'dotenv'
 // import IUser from '../types/usermodeltypes'
 dotenv.config({ path: '../../config/.env' });
 
-const interface UserDocument extends mongoose.Document{
+export interface IUserDocument extends mongoose.Document{
     email: string;
     name: string;
     password: string;
     createdAt: Date;
     updatedAt: Date;
-    comparePassword(candiidatePassword: string): Promise<boolean>;
+    comparePassword(candidatePassword: string): Promise<boolean>;
 }
 const UserSchema = new mongoose.Schema(
     {
@@ -23,6 +23,30 @@ const UserSchema = new mongoose.Schema(
     }
 )
 
-const User = mongoose.model("User", UserSchema)
+UserSchema.pre("save", async function(next: mongoose.HookNextFunction){
+    // eslint-disable-next-line prefer-const
+    let user = this as IUserDocument
+
+    if (!user.isModified("password")) return next()
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const salt = await bcrypt.genSalt(process.env.SALT!)
+
+    const hash = await bcrypt.hashSync(user.password, salt)
+    
+    user.password = hash
+    
+    return next()
+})
+
+//login schema, compares plaintext password with bcrypt hashed password
+UserSchema.methods.comparePassword = async function(
+    candidatePassword: string){
+        const user = this as IUserDocument
+        return bcrypt.compare(candidatePassword, user.password).catch((e) => false)
+    }
+
+
+const User = mongoose.model<IUserDocument>("User", UserSchema)
 
 export default User
