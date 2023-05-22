786da441d3e1ac99c578e0b3f3a5941c9ea506c7
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-22 23:00:59 +0100

    refactor: :construction:

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,17 @@
 import { Express, Request, Response } from "express"
+import { createUserHandler } from "../controller/user.controller"
 export default function (app: Express){
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200))
-    
+
+    //Register User
+    app.post("/api/users", validateRequest(createUserSchema), createUserHandler)
+
+    //Login User
+    app.get()
+
+    //User sessions
+    app.get()
+
+    //Logout
+    app.delete()
 }
