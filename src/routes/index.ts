65d9c4d233b6ee68b82b6d9a4ac32e362c00072c
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-25 22:12:41 +0100

    login route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Express, Request, Response } from "express"
-import { createUserHandler } from "../controller/user.controller"
+import { createUserHandler, createUserSessionHandler } from "../controller/user.controller"
 import validateRequest from '../middleware/validateRequest'
-import (createUserSchema) from '../schema/user.schema'
+import {createUserSchema, createUserSessionSchema} from '../schema/user.schema'
 export default function (app: Express){
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200))
 
@@ -9,7 +9,7 @@ export default function (app: Express){
     app.post("/api/users", validateRequest(createUserSchema), createUserHandler)
 
     //Login User
-    app.post()
+    app.post( "/api/sessions", validateRequest(createUserSessionSchema), createUserSessionHandler)
 
     //User sessions
     app.get()
