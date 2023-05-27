5543a4ee3fc1e3b8fe3f1275acbf61460d83ad18
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-27 22:46:39 +0100

    fixed session model

--- src/model/session.model.ts
+++ src/model/session.model.ts
@@ -1,6 +1,22 @@
 import mongoose from 'mongoose';
-import { UserDocument } from './user/model'
+import { IUserDocument } from './user.model'
 
 export interface SessionDocument extends mongoose.Document{
-    user: 
+    user: IUserDocument["_id"];
+    valid: boolean;
+    userAgent: string;
+    timestamps: Date
 }
+
+const SessionSchema = new mongoose.Schema(
+    {
+        user: {type: mongoose.Schema.Types.ObjectId, ref: "user"},
+        valid: {type: Boolean, default: true},
+userAgent:{type: String},
+    },
+    {timestamps: true}
+)
+
+const Session = mongoose.model<SessionDocument>("Session", SessionSchema)
+
+export default Session
