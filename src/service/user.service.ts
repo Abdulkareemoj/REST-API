9a83694dffc95aaa94e57d2cda18c65ad2ca76c1
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-27 22:49:00 +0100

    completed user validation service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,5 +1,6 @@
 import DocumentDefinition from "mongoose"
 import User, {IUserDocument} from '../model/user.model'
+import { omit } from "lodash"
 
 export async function createUser(input: DocumentDefinition<IUserDocument>){
     try{
@@ -9,6 +10,26 @@ export async function createUser(input: DocumentDefinition<IUserDocument>){
         throw new Error(error)
     }
 }
-function findUser(){
-    try 
+function findUser() {}
+
+export async function validatePassword({
+    email,
+     password,
+}: {
+   email: IUserDocument["email"]
+   password: string
+
+
+})
+{
+
+    const user = await User.findOne({email})
+    if (!user){
+        return false
+    }
+    const isValid = await user.comparePassword(password)
+    if (!isValid){
+        return false
+    }
+    return omit(user.toJSON(), "password")
 }
