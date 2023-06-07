91ccd56f8d5d0cb69e828972098e8a856bbfd830
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-07 21:48:28 +0100

    Logout route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import { Express, Request, Response } from "express"
 import { createUserHandler } from "../controller/user.controller"
-import { createUserSessionHandler } from "../controller/session.controller"
-import validateRequest from '../middleware/validateRequest'
+import { createUserSessionHandler, invalidateUserSessionHandler } from "../controller/session.controller"
+import {validateRequest,  requiresUser} from '../middleware'
 import {createUserSchema, createUserSessionSchema} from '../schema/user.schema'
 export default function (app: Express){
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200))
@@ -16,5 +16,5 @@ export default function (app: Express){
     // app.get()
 
     //Logout
-    // app.delete()
+    app.delete("/api/session", requiresUser, invalidateUserSessionHandler)
 }
