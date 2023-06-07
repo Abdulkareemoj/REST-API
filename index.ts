8aa2bcc1aaa7f33db6a0dfbc892d761b3ebacbc6
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-07 21:46:02 +0100

    add desrialization for users

--- index.ts
+++ index.ts
@@ -5,14 +5,16 @@ import log from "./src/logger"
 import connect from "./config/connect";
 import routes from "./src/routes"
 dotenv.config({ path: './config/.env' });
-
+import {deserializeUser} from "./src/middleware"
 
 const app = express();
 const port = process.env.PORT
 const base = process.env.BASE
 
-app.use(cors()); 
-app.use(express.json());
+// app.use(cors()); 
+// app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(deserializeUser)
 
 app.listen(port, () => {
  log.info(`[INFO] Server Started on PORT: ${port}`);
