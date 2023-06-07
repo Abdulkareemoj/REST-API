1e7a78e2c671d2590da41da34b094b4897985fae
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-07 21:49:10 +0100

    update session function

--- src/service/session.service.ts
+++ src/service/session.service.ts
@@ -3,11 +3,13 @@ import UserDocument from '../model/user.model'
 import {sign} from '../utils/jwt.utils'
 import {get} from 'lodash'
 import dotenv from 'dotenv'
+import { findUser } from "./user.service"
+import { FilterQuery, UpdateQuery } from "mongoose"
 dotenv.config({ path: '../../config/.env' });
 
 const accessToken = process.env.ACCESSTOKEN as string
 
-console.log(äccessToken)
+// console.l/og(accessToken)
 export async function createSession(userId: string, userAgent: string){
     const session = await Session.create({user: userId, userAgent})
 
@@ -38,15 +40,16 @@ export async function reIssueAccessToken({
 }: {
     refreshToken: string
 })
+{
 
 const {decoded} = decode(refreshToken)
 if(!decoded || !get(decoded, "_id")) return false
 
 //get session
-const session = await Ssession.findById(get(decoded, "_id"))
+const session = await Session.findById(get(decoded, "_id"))
+
 
 //check if session is valid
-
 if(!session || !session?.valid) return false
 
 const user = await findUser({ _id: session.user })
@@ -57,5 +60,11 @@ const accessToken = createAccessToken({ user, session })
 
 
 return accessToken
+}
 
+export async function updateSession(
+    query: FilterQuery<SessionDocument>,
+    update: UpdateQuery<SessionDocument>
+){
+    return Session.updateOne(query, update)
 }
