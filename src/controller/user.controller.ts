edd74a1b8c23bd0e46188305386d9625ea55b8db
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-08 11:19:44 +0100

    fixed error catch

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from "express";
 import {omit} from "lodash"
 import {createUser} from '../service/user.service'
-import log from '../logger'
-export async function createUserHandler(res:Request, req:Response){
+// import log from '../logger'
+export async function createUserHandler(req:Request, res:Response){
     try{
         const user = await createUser(req.body)
    return res.send(omit(user.toJSON(), "password"))
+    //look at previous commit incase this doesnt work out well
+   } catch (e: unknown) {
+    if (e instanceof Error) {
+      return res.status(409).send(e.message);
     }
-    catch(e){
-return res.status(409).send(e.message)
-    }
-
+    return res.status(500).send("Internal Server Error");
+  }
 }
