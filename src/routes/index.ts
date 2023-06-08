d53b7e5bb5148f591e7ebc293d7ac8fe7636bc38
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-08 11:22:00 +0100

    added user sessions route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Express, Request, Response } from "express"
 import { createUserHandler } from "../controller/user.controller"
-import { createUserSessionHandler, invalidateUserSessionHandler } from "../controller/session.controller"
-import {validateRequest,  requiresUser} from '../middleware'
+import { createUserSessionHandler, invalidateUserSessionHandler, getUserSessionsHandler } from "../controller/session.controller"
+import validateRequest from '../middleware/validateRequest'
+import  requiresUser from '../middleware/requiresUser'
 import {createUserSchema, createUserSessionSchema} from '../schema/user.schema'
 export default function (app: Express){
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200))
@@ -13,7 +14,7 @@ export default function (app: Express){
     app.post( "/api/sessions", validateRequest(createUserSessionSchema), createUserSessionHandler)
 
     //User sessions
-    // app.get()
+    app.get("/api/sessions", requiresUser, getUserSessionsHandler)
 
     //Logout
     app.delete("/api/session", requiresUser, invalidateUserSessionHandler)
