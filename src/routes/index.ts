1da255eb957c53072448cea869437f4029f390aa
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-12 02:20:31 +0100

    added post imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Express, Request, Response } from "express"
 import { createUserHandler } from "../controller/user.controller"
 import { createUserSessionHandler, invalidateUserSessionHandler, getUserSessionsHandler } from "../controller/session.controller"
+import { createPostHandler, getPostHandler, updatePostHandler, deletePostHandler} from '../controller/post.controller'
 import validateRequest from '../middleware/validateRequest'
 import  requiresUser from '../middleware/requiresUser'
 import {createUserSchema, createUserSessionSchema} from '../schema/user.schema'
+import {createPostSchema, updatePostSchema, deletePostSchema} from '../schema/post.schema'
+
 export default function (app: Express){
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200))
 
