d485488a40cc71c07f6aa7203784ac043d42f739
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-22 14:32:37 +0100

    added logger

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,13 +1,14 @@
 import { Request, Response } from "express";
 import {omit} from "lodash"
 import {createUser} from '../service/user.service'
-// import log from '../logger'
+import log from '../logger'
 export async function createUserHandler(req:Request, res:Response){
     try{
         const user = await createUser(req.body)
    return res.send(omit(user.toJSON(), "password"))
-    //look at previous commit incase this doesnt work out well
+    //look at previous commit incase this doesnt work out well edd74a1
    } catch (e: unknown) {
+    log.error(e)
     if (e instanceof Error) {
       return res.status(409).send(e.message);
     }
