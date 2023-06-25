c43fc270c35f92f8fd4c2fb6ff6ca12204e58141
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-25 22:23:42 +0100

    separated to another schema

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -21,15 +21,3 @@ export const createUserSchema = object({
 });
 
 export type CreateUserInput = Omit<TypeOf<typeof createUserSchema>, "body.passwordConfirmation">
-
-export const createUserSessionSchema = object({
-  body: object({
-    name: string({ required_error: "Name is required" }),
-    password: string({ required_error: "Password is required" })
-      .min(6, "Password is too short - 6 Characters Minimum")
-      .matches(/^[a-zA-Z0-9_.-]*$/, "Password can only contain letters"),
-    email: string({ required_error: "Email is required" }).email(
-      "Must be a valid email"
-    ),
-  }),
-});
