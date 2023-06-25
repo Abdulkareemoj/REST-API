53956ac764c86f6f8361f80556b438dd1848902a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-06-25 22:18:05 +0100

    update jwt calls

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -2,14 +2,15 @@ import { validatePassword } from "../service/user.service";
 import { Request, Response } from "express";
 import dotenv from 'dotenv';
 dotenv.config({ path: '../../config/.env' });
- import { createSession, createAccessToken, updateSession } from "../service/session.service"
+ import { createSession, findSessions, updateSession } from "../service/session.service" //createAccessToken
 import {get} from "lodash"
+import { signJwt } from "../utils/jwt.utils";
 
 export async function createUserSessionHandler(req: Request, res: Response){
 
 
   
-const refreshtoken = process.env.REFRESHTOKEN as string
+const refreshtkn = process.env.REFRESHTOKEN as string
 
 
     //Validate the email and password
@@ -22,16 +23,18 @@ if (user){
 const session = await createSession(user._id, req.get("user-agent") || "")
 
     //Create access token
-
-const accessToken = createAccessToken({
-    user,
-    session,
-})
+ const accessToken = signJwt(
+    {...user,
+    session: session._id } 
+    // {expiresIn: }
+    )
     //Create refresh token
 
-const refreshToken = sign(session,{
-    expiresIn: process.env.refreshtoken //1 year
-})
+const refreshToken = signJwt(
+    {...user,
+    session: session._id } 
+    // {expiresIn: } //1 year
+)
 
     //Send access and refresh token back
     return res.send({accessToken, refreshToken})
@@ -39,13 +42,19 @@ const refreshToken = sign(session,{
 
 export async function invalidateUserSessionHandler(req: Request, res: Response){
     const sessionId = get(req, "user.session")
-    await updateSession({_id: sessionId}, {value: false})
-    return res.sendStatus(200)
+    await updateSession({_id: sessionId}, {valid: false})
+    return res.sendStatus({
+        accessToken: null,
+        refreshToken: null,
+        //typescript added all this, idont know why, but if its breaking change the return to sendstatus(200)
+        
+        
+    })
 }
 
 
 export async function getUserSessionsHandler(req: Request, res: Response){
-const userId = get (req, "user_.id")
+const userId = res.locals.user._id
 const sessions = await findSessions({user: userId, valid: true})
 return res.send(sessions)
 }
